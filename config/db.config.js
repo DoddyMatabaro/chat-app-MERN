9c06d336a777a2efe0113413f858e80bb36d7a59
Doddy MATABARO <matabarododdy@gmail.com>
2022-11-08 23:52:21 +0200

    serialise mysql commands

--- config/db.config.js
+++ config/db.config.js
@@ -1,20 +1,57 @@
 
 const mysql= require('mysql');
-const connection = mysql.createConnection({
+const transaction =  require('node-mysql-transaction');
+var mkdirp = require('mkdirp');
+var crypto = require('crypto');
+
+
+const trCon = transaction({
+  connection : [mysql.createConnection,{
   host     : 'localhost',
   user     : 'root',
   password : '',
   database: 'test'
-});
+}],
+dynamicConnection: 32,
+idleConnectionCutoffTime: 1000,
+timeout:600});
 
-connection.connect((err,db)=>{
+trCon.set((err, db)=>{
+  db.query("CREATE TABLE IF NOT EXISTS users ( \
+    id INTEGER PRIMARY KEY, \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB, \
+    name TEXT, \
+    email TEXT UNIQUE, \
+    email_verified INTEGER \
+  )", [],(err, result)=>{
+    if(err) db.rollback()
+  });
+
+  db.query("CREATE TABLE IF NOT EXISTS federated_credentials ( \
+    id INTEGER PRIMARY KEY, \
+    user_id INTEGER NOT NULL, \
+    provider TEXT NOT NULL, \
+    subject TEXT NOT NULL, \
+    UNIQUE (provider, subject) )",[],(err, result)=>{
+      if(err) db.rollback()
+    });
+
+  const salt = crypto.randomBytes(16);
+
+  db.query('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+      'doddy',
+      crypto.pbkdf2Sync("king", salt, 310000, 32, 'sha256'),
+      salt
+    ], (err, result)=>{
+      if(err) db.rollback()
+  });
   if(err) throw err;
     console.log(err);
-
-
 });
 
-module.exports = connection;
+module.exports = trCon;
 
 // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 //   if (err) throw err;
